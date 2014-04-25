ff90c9ea931ca669f339696299ab1bbddb39f549
Josh Marchán <sykopomp@sykosomatic.org>
2014-04-24 17:38:45 -0700

    Use a link tag instead of style, to get sourcemaps to work

--- ChunkWithStyleTagTemplate.js
+++ ChunkWithStyleTagTemplate.js
@@ -15,16 +15,22 @@ ChunkWithStyleTagTemplate.prototype.render = function(chunkA, chunkB) {
 	var source = new ConcatSource();
 	if (chunk._chunkStyles) {
 		var fnText = insertCss.toString();
-		source.add("(("+fnText+")("+JSON.stringify(chunk._chunkStyles)+"));");
+		source.add("(("+fnText+")("+JSON.stringify(chunk._chunkStyles)+"));\n");
+		source.add("function ___go_js_go_go_go___() {\n");
 	}
 	source.add(this.chunkTemplate.render.apply(this.chunkTemplate, arguments));
+	if (chunk._chunkStyles) {
+		source.add("\n}");
+	}
 	source.rendered = true;
 	return source;
 };
 
 function insertCss(css) {
-	var style = document.createElement("style");
-	style.setAttribute("type", "text/css");
-	style.appendChild(document.createTextNode(css));
-	document.getElementsByTagName("head")[0].appendChild(style);
+	var link = document.createElement("link");
+	link.setAttribute("rel", "stylesheet");
+	link.setAttribute("type", "text/css");
+	link.setAttribute("href", "data:text/css;base64,"+btoa(css));
+	link.addEventListener("load", ___go_js_go_go_go___);
+	document.getElementsByTagName("head")[0].appendChild(link);
 }
